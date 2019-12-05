fdaa7fd0e1cdd8d821b28e33b42380cc108fe591
Christopher Lamm <chris@theantichris.com>
2019-12-05 07:35:00 -0500

    added size to the queue

--- queue.py
+++ queue.py
@@ -1,9 +1,23 @@
 from node import Node
 
 class Queue:
-    def __init__(self):
+    def __init__(self, max_size=None):
         self.head = None
         self.tail = None
+        self.max_size = max_size
+        self.size = 0
 
     def peek(self):
         return self.head.get_value()
+
+    def get_size(self):
+        return self.size
+
+    def has_space(self):
+        if self.max_size == None:
+            return True
+        else:
+            return self.max_size > self.get_size()
+
+    def is_empty(self):
+        return self.get_size() == 0
